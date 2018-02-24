232777fa6d809cf7be45bb12b2ff30c621fcfbf1
Sabiha Mahek <smahmed@andrew.cmu.edu>
2018-02-24 18:13:24 -0500

    tfidf

--- free_text2.py
+++ free_text2.py
@@ -14,3 +14,9 @@ for i,doc in enumerate(document_word):
     for word in doc:
         X_tf[i,vocab_dict[word]]+=1
 print(X_tf)
+
+idf = np.log(X_tf.shape[0]/X_tf.astype(bool).sum(axis=0))
+print(idf)
+
+X_tfidf = X_tf*idf
+print(X_tfidf)
